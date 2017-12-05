0ade43c856f0ebe9749b5beaae54fb66031084b9
cnbs5 <879700945@qq.com>
2017-12-05 14:31:34 +0800

    Initial commit

---
+++ app/src/main/java/com/ck/txccar/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.ck.txccar
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by cnbs5 on 2017/12/5.
+ */
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ck/txccar/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.ck.txccar
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_contact -> {
+                message.setText(R.string.title_contact)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_buy -> {
+                message.setText(R.string.title_buy)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_send -> {
+                message.setText(R.string.title_send)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_my -> {
+                message.setText(R.string.title_my)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
