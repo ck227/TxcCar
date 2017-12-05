1ac1b84ca02fa961b3add3b45a5134038da404b6
cnbs5 <879700945@qq.com>
2017-12-05 17:49:57 +0800

    login screen

--- app/src/main/java/com/ck/txccar/LoginActivity.kt
+++ app/src/main/java/com/ck/txccar/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.ck.txccar
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_login.*
 
 /**
  * Created by cnbs5 on 2017/12/5.
@@ -11,5 +13,11 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_login)
+
+        login.setOnClickListener {
+//            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            
+        }
     }
 }
