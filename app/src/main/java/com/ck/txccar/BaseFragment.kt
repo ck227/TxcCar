0948df4c79b7e19079b412f2f085dd4a9df049ad
cnbs5 <879700945@qq.com>
2017-12-06 17:06:47 +0800

    稳一波

---
+++ app/src/main/java/com/ck/txccar/BaseFragment.kt
@@ -0,0 +1,74 @@
+package com.ck.txccar
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import kotlinx.android.synthetic.main.fragment_base.*
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.ck.txccar.R.id.webView
+
+
+/**
+ * Created by cnbs5 on 2017/12/6.
+ */
+
+class BaseFragment : Fragment() {
+
+//    var webView: WebView? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_base, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val url = arguments.getString("url")
+
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.builtInZoomControls = true
+        webView.settings.useWideViewPort = true
+        webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+        webView.settings.loadWithOverviewMode = false
+        webView.settings.allowContentAccess = false
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            //覆盖shouldOverrideUrlLoading 方法
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl(url)
+    }
+
+    private val PARAM = "PARAM"
+
+    companion object {
+        fun newInstance(url: String): BaseFragment {
+            var args: Bundle = Bundle()
+            args.putString("url", url)
+            var editFragment: BaseFragment = newInstance()
+            editFragment.arguments = args
+            return editFragment
+        }
+
+        fun newInstance(): BaseFragment {
+            return BaseFragment()
+        }
+    }
+
+    fun getWebView(): WebView {
+        if (webView == null) {
+            Log.e("k", "sdfsfsfsf")
+        }
+        return webView
+    }
+}
--- app/src/main/java/com/ck/txccar/MainActivity.kt
+++ app/src/main/java/com/ck/txccar/MainActivity.kt
@@ -2,31 +2,52 @@ package com.ck.txccar
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val baseUrl = "http://192.168.100.222:8080/txcCar/"
+    var baseFragment: BaseFragment? = null
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                supportFragmentManager.inTransaction {
+                    baseFragment = BaseFragment.newInstance(baseUrl + "indexHtml/index.html")
+                    replace(R.id.content, baseFragment, "TAG")
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_contact -> {
-                message.setText(R.string.title_contact)
+                supportFragmentManager.inTransaction {
+                    baseFragment = BaseFragment.newInstance(baseUrl + "addressBookHtml/list.html")
+                    replace(R.id.content, baseFragment, "TAG")
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_buy -> {
-                message.setText(R.string.title_buy)
+                supportFragmentManager.inTransaction {
+                    baseFragment = BaseFragment.newInstance(baseUrl + "wantToBuyHtmlHtml/list.html")
+                    replace(R.id.content, baseFragment, "TAG")
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_send -> {
-                message.setText(R.string.title_send)
+                supportFragmentManager.inTransaction {
+                    baseFragment = BaseFragment.newInstance(baseUrl + "categoryDetailsHtml/add.html?loginName=15717174872")
+                    replace(R.id.content, baseFragment, "TAG")
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_my -> {
-                message.setText(R.string.title_my)
+                supportFragmentManager.inTransaction {
+                    baseFragment = BaseFragment.newInstance(baseUrl + "userHtml/user.html?loginName=15717174872 ")
+                    replace(R.id.content, baseFragment, "TAG")
+                }
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -38,5 +59,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation)
+        bottomNavigationView.selectedItemId = R.id.navigation_home
     }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && baseFragment!!.getWebView()!!.canGoBack()) {
+            baseFragment!!.getWebView()!!.goBack()// 返回前一个页面
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
